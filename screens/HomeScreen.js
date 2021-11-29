db33e8ab3e33172eea2e3b378ee5dc862bde7ad0
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-11-29 15:22:19 -0500

    set up .env file with google api; installed google places autocomplete to make the user experience easier; configured babel for the api key

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../componets/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { API_KEY } from "@env"
 
 const HomeScreen = () => {
+
+    
+
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
@@ -16,6 +21,31 @@ const HomeScreen = () => {
                     source={
                         { uri: 'https://links.papareact.com/gzs', }
                     }
+                />
+                <GooglePlacesAutocomplete 
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 15,
+                        },
+                    }}
+                    onPress={(data, details = null) => {
+                        console.log(data, details)
+                    }}
+                    query={{
+                        // google places API autocomplete search query
+                        key: API_KEY,
+                        language: 'en',
+                    }}
+                    placeholder='Where From?'
+                    nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+                    debounce={400} // debounce adds a delay before the search is executed
+                    enablePoweredByContainer={false} // enablePoweredByContainer gets rid of the branding at the bottom of the screen.
+                    fetchDetails={true} // fetchDetails enables the fetching of details for each search result
+                    minLength={2}  // minimum length of text to search
+                    returnKeyType='search' // when return is pressed, search is then executed
                 />
 
                 <NavOptions />
