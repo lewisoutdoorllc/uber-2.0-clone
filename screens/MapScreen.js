4b4de6e13709a4c46056d275603e520bfceb0f6c
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-11-30 12:11:45 -0500

    Revert "set up navigation card and linked the ride options component; map screen is not quite working like I want so I will need to work on that a little more; everything is rendering and working properly at this time"
    This reverts commit 3b88a20b91e06d3d9a6cdc06d1485274139a8652.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,40 +1,18 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'tailwind-react-native-classnames'
 import Map from '../componets/Map'
-import NavigateCard from '../componets/NavigateCard'
-import RideOptionsCard from '../componets/RideOptionsCard'
 
 
 const MapScreen = () => {
-
-    const Stack = createNativeStackNavigator();
-
     return (
         <SafeAreaView>
-            <View style={tw`h-1/4`}>
+            <View style={tw`h-1/2`}>
                 <Map />
             </View>
-
-            <View style={tw`h-3/4`}>
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name='NavigateCard'
-                        component={NavigateCard}
-                        options={{
-                            headerShown: false
-                        }}
-                    />
-                    <Stack.Screen
-                        name='RideOptionsCard'
-                        component={RideOptionsCard}
-                        options={{
-                            headerShown: false
-                        }}
-                    />
-                </Stack.Navigator>
+            <View style={tw`h-1/2 bg-red-500`}>
+                <Text>Here is the map screen!!</Text>
             </View>
         </SafeAreaView>
     )
